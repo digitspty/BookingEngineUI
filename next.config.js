725b8262071da41dbb0e0f2efbd49ab9784c4b68
digitspty <jmgradpanama@gmail.com>
2024-11-18 15:40:01 -0500

    Update dependencies and configuration

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
+const { withCloudflarePages } = require('@cloudflare/next-on-pages/plugin');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
+  /* experimental: {
     appDir: true,
   },
-  reactStrictMode: true,
+  reactStrictMode: true, */
+  output: "export",
 };
 
+module.exports = withCloudflarePages({
+  nextConfig
+});
 // SAAS next.config.js
 const withSass = require('@zeit/next-sass');
 module.exports = withSass();
